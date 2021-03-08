bda787e672b400e923aafec7fa012723d675df80
Adriana Oliveira <adripdv@gmail.com>
2021-03-08 17:21:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# from file import class 
+from animal import Animal
+
+# instance / object
+dog = Animal(name='Lizzy', age=3)
+cat = Animal(name='Zorka', age=10)
+bird = Animal(name='Fufu', age=12)
+
+print(dog.speak())
+print(cat.speak())
+print(bird.speak())
+
+
+# print(Animal.__dict__)
